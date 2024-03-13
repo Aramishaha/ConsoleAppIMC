aedd035b8934651c38b4abe1675e109dc0d09eea
MOHAMED ARAMIS Aramis <amohamed@edouardgand.fr>
2024-03-13 15:33:07 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppIMC/Profil.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleAppIMC
+{
+    public class  Profil 
+    {
+        protected string nom;
+        protected string prenom;
+        protected int poids;
+        protected int taille;
+
+
+        public Profil(string unNom, string unPrenom, int unPoids, int uneTaille)
+        {
+            nom = unNom;
+            prenom = unPrenom;
+            poids = unPoids;
+            taille = uneTaille;
+        }
+
+        public float calculIMC()
+        {
+            float tailleCm = (float)taille / 100;
+            return (poids / (tailleCm * tailleCm));
+        }
+
+
+
+
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleAppIMC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            Profil profil = new Profil("Jérôme", "Dubromelle", 80, 180);
+            var resultatAttendu = 24.69;
+            //Act
+            var resultatTest = profil.calculIMC();
+            //Assert
+            Assert.AreEqual(resultatAttendu, profil.calculIMC(), delta: 0.1);
+        }
+    }
+}
